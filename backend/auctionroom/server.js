6829bc8e752435f61c9d63e8837702a14578e4fc
simran-bhadani3 <simranbhadani333@gmail.com>
2021-10-16 18:33:52 +0800

    no message

---
+++ backend/auctionroom/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const auctionRoom = require("./src/routes/AuctionRoom");
+
+app.use("/api/room", auctionRoom);
+
+app.listen(port, function (err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("connected");
+	}
+});
