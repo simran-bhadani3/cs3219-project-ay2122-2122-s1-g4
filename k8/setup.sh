e95787c419cb4a550ad09855935b541eceab730c
simran-bhadani3 <49784707+simran-bhadani3@users.noreply.github.com>
2021-10-28 20:09:04 +0800

    Update setup.sh

--- k8/setup.sh
+++ k8/setup.sh
@@ -1,9 +1,30 @@
 # commands to get all our kube resources up 
+kubectl apply -f ./k8/services/1-frontend.yaml 
+kubectl apply -f ./k8/services/2-mongodb.yaml 
+kubectl apply -f ./k8/services/3-useraccount.yaml 
+kubectl apply -f ./k8/services/4-auctionroommanager.yaml 
+kubectl apply -f ./k8/services/5-accountdetails.yaml
+kubectl apply -f ./k8/ingress/unauth-ingress.yaml 
 
 # clean up
-kubectl delete -f "./k8/services/auctiondetails.yaml"
-kubectl delete -f "./k8/controllers/auctiondetails.yaml"
+kubectl delete deployment frontend
+kubectl delete service frontend
 
-kubectl apply -f "./k8/services/auctiondetails.yaml"
-kubectl apply -f "./k8/controllers/auctiondetails.yaml"
-kubectl rollout status deployment/auctiondetails
+kubectl delete deployment useraccount
+kubectl delete service useraccount
+
+kubectl delete deployment auctionroommanager
+kubectl delete service auctionroommanager
+
+kubectl delete deployment accountdetails
+kubectl delete service accountdetails
+
+kubectl delete deployment mongodb
+kubectl delete service mongodb
+
+kubectl delete -f ./k8/ingress/unauth-ingress.yaml 
+
+# kubectl rollout status deployment/auctiondetails
+# kubectl get ingress  --watch
+# kubectl delete ing auth-ingress
+# kubectl delete ing unauth-ingress
