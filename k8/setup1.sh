920828b866b4b8f99982d17adbea497ef064e4ba
davidcaiqifan <caid100@gmail.com>
2021-11-01 01:00:07 +0800

    Add cors to auth-ingress and add delay timer to auctionroommanager service

--- k8/setup1.sh
+++ k8/setup1.sh
@@ -6,7 +6,6 @@ kubectl apply -f ./k8/services/2-mongodb.yaml;
 # redis server needs to start before auctionroom
 kubectl apply -f ./k8/services/7-redis.yaml ;
 kubectl apply -f ./k8/services/3-useraccount.yaml; 
-kubectl apply -f ./k8/services/4-auctionroommanager.yaml; 
 kubectl apply -f ./k8/services/5-auctiondetails.yaml;
 kubectl apply -f ./k8/ingress/unauth-ingress.yaml ;
 kubectl apply -f ./k8/ingress/auth-ingress.yaml ;
@@ -14,3 +13,6 @@ kubectl apply -f ./k8/ingress/chat-ingress.yaml ;
 # run this if frontend container has not been built(if the first time running this script)
 # docker run --publish 8080:3000 --name auctiondocker -i  -t auctionfrontend:latest  
 docker container start auctiondocker
+# wait for redis to start first
+sleep 5s
+kubectl apply -f ./k8/services/4-auctionroommanager.yaml; 
