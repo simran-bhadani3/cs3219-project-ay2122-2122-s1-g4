265e754fd236235e2c7667e5ba873c62e23cf8c8
ppris <49683462+pPris@users.noreply.github.com>
2021-11-01 00:14:11 +0800

    Add docker, yaml files for agg svr
    # Status - inside docker: connection error to localhost

--- k8/aggregate/aggregate-svr/server.js
+++ k8/aggregate/aggregate-svr/server.js
@@ -7,40 +7,57 @@ const endpoint1 = "/api/auctiondetails";
 const endpoint2 = "/api/currency";
 app.use(express.json());
 
-// app.use("/", entry);
 
 app.get("/api/agg/:userId", async (req, res) => {
     try {
-		// const createdAuctions = await axios({
-        //     // todo do we need to get all auction rooms by user? 
-		// 	url: `${hostUrl}${endpoint1}/userId`,
-		// 	method: "get",
-		// });
-        // const currentBalance = 
-        // const userDetails //?
-
-        // const testGetAllAuctions = await axios({
-		// 	url: `${hostUrl}${endpoint1}`,
-		// 	method: "get",
-		// });
+        console.log(`received req: ${JSON.stringify(req.url)}`);
         
-        const testGetFrontend = await axios({
-			url: `${hostUrl}/test-auth`,
+        const jsonResponseFromTestApi = await axios({
+            host: "kubernetes.docker.internal", //"localhost",
+            url: "/test-auth",
 			method: "get",
-		});
+		})
+        .catch(axiosRes => {
+            // bug: there might be other errors than just a not 200 ok status code
+            // e.g. cant connect to host
+            // so need to handle them differently...
+            console.log(axiosRes)
+            const json = {
+                "data": axiosRes.response.data,
+                "status": axiosRes.response.status,
+                "message": `${axiosRes.response.status} ${axiosRes.response.statusText}`
+            };
+            return json;
+        });
 
-        const combine = testGetFrontend;
+        console.log(`test: ${jsonResponseFromTestApi}`)
+        const combineJson = {
+            "testApi": jsonResponseFromTestApi,
+            "userApi": null, 
+            "currencyApi": null
+        }; // todo we need to think about how to format the aggregated response
 
-		res.status(200).json(combine.data);
+		res.status(200).json(combineJson);
 	} catch (err) {
         console.log(err);
-		res.status(500).json({ message: err });
+		res.status(500).send(err.message);
 	}
 })
 
 
-const port = process.env.PORT || 8085; 
+app.get("/api/test-agg", async (req, res) => {
+    try {
+        const url = "localhost/test-auth";
+        const data = await fetch(url)
+            .then(response => response.json())
 
+        res.send(data);
+    } catch (ex) {
+        res.status(500).send(ex.message)
+    }
+})
+
+const port = process.env.PORT || 8085; 
 
 const server = app.listen(port, () => {
     console.log(`listening on port ${port}...`);
