34edcb37eb17583afdb9dc9ddb1dc7f215707e09
davidcaiqifan <caid100@gmail.com>
2021-11-04 12:17:09 +0800

    Added api routes for retrieving auction details by user, price, time and category

--- backend/auctiondetails/src/models/auctiondetailmodel.js
+++ backend/auctiondetails/src/models/auctiondetailmodel.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const uuid = require('mongoose-uuid');
 const AuctionDetailSchema = mongoose.Schema({
     room_display_name: {type: String, required: [true, 'display name required']},
-    // roomid: {type: String, index: true, unique: true},
     auction_item_name: {type: String, required: [true, 'item name required']},
     owner_id: {type: String, required: [true, 'valid owner id required']},
     start_time: {type: Date, required: [true, 'valid date string required for end time']},
     end_time: {type: Date, required: [true, 'valid date string required for end time']},
     description: {type: String},
+    category: {type: String},
     increment: {type: Number, required: [true, 'increment required']},
     minbid: {type: Number, required: [true, 'minbid required']}
 }, {
