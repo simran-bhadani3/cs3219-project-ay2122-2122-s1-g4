6487d450925181bb7d53750004b23be93ad247f5
ppris <49683462+pPris@users.noreply.github.com>
2021-11-07 20:47:35 +0800

    Integrate websocket proxying
    # https://github.com/MethodGrab/socketio-proxy-boilerplate/blob/master/index.js

--- k8/aggregate/aggregateproxy/index.js
+++ k8/aggregate/aggregateproxy/index.js
@@ -1,13 +1,16 @@
 const axiosCombiner = require('./axios');
 const express = require('express');
 const morgan = require("morgan");
+const http = require('http');
+const httpProxy = require('http-proxy')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 
 // Create Express Server
+/** App refers to express app, that will be handling client's requests and sending it out to the next service/ */
 const app = express();
-const server = require('http').createServer(app)
+// const server = require('http').createServer(app)
 
 //enable cors
 const cors = require('cors');
@@ -34,35 +37,34 @@ app.get('/agg/test', (req, res) => {
     res.send('This is an aggregation and proxy endpoint. It seems to be up and running.');
 });
 
-// app.get('/agg/test2', (req, res) => {
-//     res.send('This is another test endpoint to trigger redployment.');
-// });
 
-// app.get('/agg/test3', (req, res) => {
-//     res.send('Hopefully last test.');
-// });
-
-app.get('/agg/test4', (req, res) => {
-    res.send('Testing after choosing cloudbuild.yaml file option.');
-});
-
-
-// Aggregate endpoints
+//// ENDPOINTS INVOLVING AGGREGATION FROM MULTIPLE APIS 
 app.use('/agg/userdetails', axiosCombiner);
  
+
+
+//// WEBSOCKET RELATED
+
+// todo should be renamed to chatRoomProxy or something after integrating the example 
+const proxy = httpProxy.createProxyServer({
+	target : `http://${FORWARDING_URL}`, // the target is to the backend, not the frontend (im guessing its the socket.io path in the apigateway)
+	// target : `http://${HOST}:${API_PORT}`, // todo help, this is the example target given. i'm not sure if its correct
+    // ws     : true,
+});
+const proxyServer = http.createServer(app);
+
 // Proxy websockets
-app.on('upgrade', function (req, socket, head) {
+proxyServer.on('upgrade', function (req, socket, head) {
     console.log("proxying upgrade request", req.url);
     proxy.ws(req, socket, head);
-  });
+});
 
 // Proxy websockets
 // https://www.twilio.com/blog/node-js-proxy-server
-app.use('/socket.io', createProxyMiddleware({
-    target: FORWARDING_URL,
-    changeOrigin: true,
-    ws: true
-}));
+const SOCKET_PATH = '/socket.io';
+app.use(`/${SOCKET_PATH}`, function ( req, res ) {
+	proxy.web( req, res, { target: `http://${FORWARDING_URL}/${SOCKET_PATH}` } );
+});
 
 // Proxy socket io
 // https://www.twilio.com/blog/node-js-proxy-server
@@ -72,19 +74,23 @@ app.use('/auctionroom', createProxyMiddleware({
     ws: true
 }));
 
-// Proxy endpoints
+
+
+//// OTHER ENDPOINTS
+
+// Forward exactly as received 
 // https://www.twilio.com/blog/node-js-proxy-server
 app.use('/api', createProxyMiddleware({
     target: FORWARDING_URL,
     changeOrigin: true,
-    ws: true
+    ws: true // todo should the ws be for these requests too or accidently added?
 }));
 
 
 
- // Start the Proxy
+//// Start this aggregating, proxying node.js server
 app.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+    console.log(`Starting nodejs server at ${HOST}:${PORT}`);
 });
  
 
