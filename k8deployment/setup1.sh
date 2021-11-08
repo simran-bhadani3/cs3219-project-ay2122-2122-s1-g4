1443950095778fcff9465fdd0cfa77fba0a0a736
simran-bhadani3 <simranbhadani333@gmail.com>
2021-11-09 00:30:53 +0800

    Resolve conflicts

--- k8deployment/setup1.sh
+++ k8deployment/setup1.sh
@@ -2,21 +2,20 @@
 # run using ./k8/setup1.sh
 # run this if nginx ingress not configured
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.4/deploy/static/provider/cloud/deploy.yaml;
-kubectl apply -f ./k8deployment/services/2-mongouseraccounts.yaml; 
-kubectl apply -f ./k8deployment/services/10-mongoauctiondetails.yaml; 
+kubectl apply -f ./k8/services/2-mongodb.yaml; 
 # redis server needs to start before auctionroom
-kubectl apply -f ./k8deployment/services/7-redis.yaml ;
-kubectl apply -f ./k8deployment/services/3-useraccount.yaml; 
-kubectl apply -f ./k8deployment/services/5-auctiondetails.yaml;
-kubectl apply -f ./k8deployment/ingress/unauth-ingress.yaml ;
-kubectl apply -f ./k8deployment/ingress/auth-ingress.yaml ;
-kubectl apply -f ./k8deployment/ingress/chat-ingress.yaml ;
-kubectl apply -f ./k8deployment/services/6-currencymanagement.yaml
-kubectl apply -f ./k8deployment/services/9-auctionroom.yaml
+kubectl apply -f ./k8/services/7-redis.yaml ;
+kubectl apply -f ./k8/services/3-useraccount.yaml; 
+kubectl apply -f ./k8/services/5-auctiondetails.yaml;
+kubectl apply -f ./k8/ingress/unauth-ingress.yaml ;
+kubectl apply -f ./k8/ingress/auth-ingress.yaml ;
+kubectl apply -f ./k8/ingress/chat-ingress.yaml ;
+kubectl apply -f ./k8/services/6-currencymanagement.yaml
+kubectl apply -f ./k8/services/9-auctionroom.yaml
 # run this if frontend container has not been built(if the first time running this script)
 docker rm auctiondocker
 docker run -d --publish 8080:3000 --name auctiondocker -i  -t auctionfrontend:latest  
 # docker container start auctiondocker
 # wait for redis to start first
 sleep 5s
-kubectl apply -f ./k8deployment/services/4-auctionroommanager.yaml; 
+kubectl apply -f ./k8/services/4-auctionroommanager.yaml; 
--- k8deployment/teardown.sh
+++ k8deployment/teardown.sh
@@ -1,15 +1,14 @@
 #!/bin/bash 
 # ./k8/teardown.sh
-# kubectl delete -f ./k8deployment/services/1-frontend.yaml;
-# kubectl delete -f ./k8deployment/services/2-mongouseraccounts.yaml; 
-# kubectl delete -f ./k8deployment/services/10-mongoauctiondetails.yaml; 
-kubectl delete -f ./k8deployment/services/3-useraccount.yaml; 
-kubectl delete -f ./k8deployment/services/4-auctionroommanager.yaml; 
-kubectl delete -f ./k8deployment/services/5-auctiondetails.yaml;
-kubectl delete -f ./k8deployment/services/6-currencymanagement.yaml
-kubectl delete -f ./k8deployment/services/7-redis.yaml;
-kubectl delete -f ./k8deployment/services/9-auctionroom.yaml
-kubectl delete -f ./k8deployment/ingress/unauth-ingress.yaml ;
-kubectl delete -f ./k8deployment/ingress/auth-ingress.yaml ;
-kubectl delete -f ./k8deployment/ingress/chat-ingress.yaml ;
+# kubectl delete -f ./k8/services/1-frontend.yaml;
+# kubectl delete -f ./k8/services/2-mongodb.yaml; 
+kubectl delete -f ./k8/services/3-useraccount.yaml; 
+kubectl delete -f ./k8/services/4-auctionroommanager.yaml; 
+kubectl delete -f ./k8/services/5-auctiondetails.yaml;
+kubectl delete -f ./k8/services/6-currencymanagement.yaml
+kubectl delete -f ./k8/services/7-redis.yaml;
+kubectl delete -f ./k8/services/9-auctionroom.yaml
+kubectl delete -f ./k8/ingress/unauth-ingress.yaml ;
+kubectl delete -f ./k8/ingress/auth-ingress.yaml ;
+kubectl delete -f ./k8/ingress/chat-ingress.yaml ;
 docker stop auctiondocker;
