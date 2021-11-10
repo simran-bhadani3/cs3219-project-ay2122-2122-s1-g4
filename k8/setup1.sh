94bbec1dafcc653c2497639e2537ca83c5d0c6a2
ppris <49683462+pPris@users.noreply.github.com>
2021-11-10 11:45:49 +0800

    Change frontend docker-compose to use aggproxy and setup1.sh to include aggproxy

--- k8/setup1.sh
+++ k8/setup1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # run using ./k8/setup1.sh
+
 # run this if nginx ingress not configured
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.4/deploy/static/provider/cloud/deploy.yaml;
+
 # changed to individual mongodb containers
 # kubectl apply -f ./k8/services/2-mongodb.yaml; 
 kubectl apply -f ./k8/services/2-mongouseraccounts.yaml; 
@@ -15,11 +17,18 @@ kubectl apply -f ./k8/ingress/auth-ingress.yaml ;
 kubectl apply -f ./k8/ingress/chat-ingress.yaml ;
 kubectl apply -f ./k8/services/6-currencymanagement.yaml
 kubectl apply -f ./k8/services/9-auctionroom.yaml
+
 # comment this out if first time running auctiondocker container(doesn't exists)
 docker rm auctiondocker;
 # run this if frontend container has not been built(if the first time running this script)
 docker run -d --publish 3000:3000 --name auctiondocker -i  -t auctionfrontend:latest  
 # docker container start auctiondocker
+
 # wait for redis to start first
 sleep 20s
 kubectl apply -f ./k8/services/4-auctionroommanager.yaml; 
+
+# start the aggregate proxy server too 
+cd ./k8/aggregate/aggregateproxy/
+docker-compose up --build -d
+cd ../../..
